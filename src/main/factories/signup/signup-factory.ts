c05e995c6224b864ff2ffd47d49381179ca9e634
Bruno Ferreira <fs.brunoferreira@gmail.com>
2020-07-22 21:16:43 -0300

    refactor: fix signup-factory and signup-validation-factory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,13 +1,18 @@
-import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from '../../../presentation/helpers/validators'
-import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
+import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
+import { DbAddAccount } from '../../../data/usescases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { makeSignUpValidation } from './signup-validation-factory'
 
-export const makeSignUpValidation = (): ValidationComposite => {
-  const validations: Validation[] = []
-  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-    validations.push(new RequiredFieldValidation(field))
-  }
-  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
-  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
-  return new ValidationComposite(validations)
+export const makeSignUpController = (): Controller => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
